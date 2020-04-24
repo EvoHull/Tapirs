4a19c7049cdeda3975924d1b08a6834674e3784e
Dave Lunt <dave.lunt@gmail.com>
2020-04-24 17:06:20 +0100

    format fixes

--- scripts/get_files_dirs.py
+++ scripts/get_files_dirs.py
@@ -3,15 +3,16 @@ import os
 
 startdir = "data/01_demultiplexed/"
 with open('samples.tsv', 'w') as outfile:
-    outfile.write('Library' + '\t' + 'Sample' + '\n') # add column headers
-    for root, dirs, files in os.walk(startdir):  # iterate across files from startdir down
+    outfile.write('Library' + '\t' + 'Sample' + '\n')  # add column headers
+    # iterate across files from startdir down
+    for root, dirs, files in os.walk(startdir):
         for file in files:
-            if file.endswith(('fastq.gz', 'fq.gz')): # specify file endings
-                path= os.path.join(root, file)
+            if file.endswith(('fastq.gz', 'fq.gz')):  # specify file endings
+                path = os.path.join(root, file)
                 head_tail = os.path.split(path)  # split into path & file
                 # remove startdir from path leaving directory name 'dirs'
                 dirs = head_tail[0].replace(startdir, '')
-                filetail = head_tail[1] # get tail, ie file
-                dflist = [dirs, filetail] # make list of directories and files
-                dfstr = '\t'.join(dflist) # convert to string with tabs
+                filetail = head_tail[1]  # get tail, ie file
+                dflist = [dirs, filetail]  # make list of directories and files
+                dfstr = '\t'.join(dflist)  # convert to string with tabs
                 outfile.write(dfstr + '\n')  # write newline after dir tab file
