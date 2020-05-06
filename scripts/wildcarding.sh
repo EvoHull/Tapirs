235a18cf0f6be8556a38ff10d2b82b837222fcc5
Dave Lunt <dave.lunt@gmail.com>
2020-05-06 18:20:31 +0100

    remove unused scripts

--- scripts/wildcarding.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -e "library\tsample" > samples.tsv
-find data/01_demultiplexed/* -name *fastq* | sed -r 's/\//\t/g' |  cut -f 3- | sed -r 's/\./\t/g' | sed -r 's/_/\t/g' | cut -f 1-2 | sort | uniq >> samples.tsv
-cut -f 1 samples.tsv | uniq >> libraries.tsv
