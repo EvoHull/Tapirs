07a9ae524cda76fbc9688f528875fb67ee69a47f
Dave Lunt <dave.lunt@gmail.com>
2020-11-27 16:24:16 +0000

    cleaning up files

--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-outfile = 'test.csv'
-# library = pd.read_csv("libraries.csv", sep='\t', header=0, index_col="library")
-sample = pd.read_csv("samples.tsv", sep='\t', header=0, index_col="library", dtype=str)
-sample.to_csv(outfile)
