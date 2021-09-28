fec33fd37ba91e797f088994eb056566f87d4fcb
Graham-Sellers <grahamssellers@gmail.com>
2021-09-28 10:32:53 +0100

    gs_update (#4): updated taxonomy_to_blast.py, mlca.py and mlca_to_tsv.py

--- workflow/scripts/mlca.py
+++ workflow/scripts/mlca.py
@@ -1,15 +1,13 @@
-# Majority Lowest Common Ancestor (mlca)
-# --------------------------------------
+# ------------------------------------------------------------------------------
+# MAJORITY LOWEST COMMON ANCESTOR (mlca)
 # caluclation of mlca from blast output
-# part of Tapirs metabarcoding workflow
+# ------------------------------------------------------------------------------
 
-# # import libraries
 import pandas as pd
 import numpy as np
+from itertools import dropwhile
 
-# input data in blast format
 infile = snakemake.input.blast
-# results file, tabular format
 outfile = snakemake.output.lca
 
 identity = float(snakemake.params.identity)
@@ -20,13 +18,16 @@ bit_threshold = float(snakemake.params.bitscore)
 prop = 1 - (bit_threshold / 100)
 
 unid_tax = ('\t'.join([str(x) for x in ['unidentified']*7]))
-taxonomy = ('kingdom','phylum','class','order','family','genus','species')
-header = 'query\tlca_rank\tlca_otu\tkingdom\tphylum\tclass\torder\tfamily\tgenus\tspecies\tmethod\n'
+taxonomy = ('domain','phylum','class','order','family','genus','species')
+header = 'query\ttax_rank\totu_id\tdomain\tphylum\tclass\torder\tfamily\tgenus\tspecies\tmethod\n'
+
+# remove trailing unknowns from lca taxonomy list
+def strip_unknown(l):
+        l = list(dropwhile(lambda x: x == 'unknown', l[::-1]))
+        return l[::-1]
 
 with open(infile, 'r') as file:
-    count=0
-    for line in file.readlines():
-        count += 1
+    count = len([1 for line in open(infile)])  # check if file is not an empty input file
     if count == 0:
         with open(outfile, 'w') as lca_out:
             lca_out.write(header)
@@ -42,30 +43,32 @@ with open(infile, 'r') as file:
                 lca = []
                 seq = df[df[0] == seq_id]
                 seq = seq[seq[7] >= max(seq[7]) * prop]
-                taxon = seq[8].drop_duplicates()
-                taxon = taxon.str.split("/", expand = True)
 
-                for col in taxon.columns:
-                    taxa = np.unique(taxon[col], return_counts = True)
-                    if max(taxa[1]) / sum(taxa[1]) >= majority / 100:
-                        lca.append(taxa[0][taxa[1] == max(taxa[1])])
+                if len(seq) >= min_hits:
+                    taxon = seq[8].drop_duplicates()
+                    taxon = taxon.str.split("/", expand = True)
+                    for col in taxon.columns:
+                        taxa = np.unique(taxon[col], return_counts = True)
+                        if max(taxa[1]) / sum(taxa[1]) >= majority / 100:
+                            lca.append(taxa[0][taxa[1] == max(taxa[1])])
 
-                lca_tax = []
-                if len(lca) == 0:
-                    lca.append('unidentified')
-                for i in lca:
-                    lca_tax.append(i[0])
-                if len(lca_tax) < 7:
-                    lca_tax = lca_tax + (['unidentified'] * (7 - len(lca_tax)))
-                else:
-                    lca_tax = lca_tax
-                lca_tax = ('\t'.join([str(x) for x in lca_tax]))
+                    if len(seq) >= min_hits and len(seq) > 1:
+                        lca_method = 'lca'
+                    elif len(seq) == 1 and min_hits == 1:
+                        lca_method = 'single_hit'
 
-                otu_id = str(lca[-1][0])
+                    lca_tax = []
+                    lca = strip_unknown(lca)
+                    if len(lca) > 0:
+                        for i in lca:
+                            lca_tax.append(i[0])
+                        if len(lca_tax) < 7:
+                            lca_tax = lca_tax + (['unidentified'] * (7 - len(lca_tax)))
+                        lca_tax = ('\t'.join([str(x) for x in lca_tax]))
 
-                if len(seq[col]) >= min_hits and len(seq[col]) > 1:
-                    lca_out.write('%s\t%s\t%s\t%s\tlca\n' %(seq_id, taxonomy[len(lca)-1], otu_id, lca_tax))
-                elif len(seq[col]) == 1 and min_hits == 1:
-                    lca_out.write('%s\t%s\t%s\t%s\tsingle_hit\n' %(seq_id, taxonomy[len(lca)-1], otu_id, lca_tax))
-                else:
-                    lca_out.write('%s\tunidentified\tunidentified\t%s\tunidentified\n' %(seq_id, unid_tax))
+                        otu_id = str(lca[-1][0])
+
+                        lca_out.write('%s\t%s\t%s\t%s\t%s\n' %(seq_id, taxonomy[len(lca)-1], otu_id, lca_tax, lca_method))
+
+                    else:
+                        lca_out.write('%s\tunidentified\tunidentified\t%s\t%s\n' %(seq_id, unid_tax, lca_method))
